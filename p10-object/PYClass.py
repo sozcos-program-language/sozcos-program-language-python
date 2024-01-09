88e515cca16d25ebdc827750415f372164fefaf3
Elsen <poozs@qq.com>
2024-01-09 17:27:20 +0800

    P10 面向对象

--- p10-object/PYClass.py
+++ p10-object/PYClass.py
@@ -13,12 +13,15 @@ class MyClass():
     var2 = 10.1
     var3 = '可乐'
 
-    def func1():
-        print("abc")
+    @classmethod # 此注解作用是让类函数可以访问类成员变量
+    def func1(cls, age):
+        print("我喜欢{}".format(cls.var3))
+        # 添加函数参数, 在调用端可以直接传此参数
+        print("今年{}岁".format(str(age)))
 
 
 print(MyClass.var1)
 print(MyClass.var2)
 print(MyClass.var3)
 
-MyClass.func1()
+MyClass.func1(18)
