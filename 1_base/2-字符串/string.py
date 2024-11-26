a1f36a6260bd8aba9a5b7cf1886d98bd83894f80
sozcos <sozcos@qq.com>
2024-11-26 21:26:02 +0800

    docs(基础): 字符串的精度控制

--- 1_base/2-字符串/string.py
+++ 1_base/2-字符串/string.py
@@ -2,3 +2,11 @@ name = "elsen"
 age = 17
 
 print("我是%s, 今年%s岁" % (name, age))
+
+
+# 字符串的精度控制
+num1 = 11.2
+num2 = 11.345
+print("补位操作 %s ==> %5d" % (num1, num1))
+print("补位四舍五入操作 %s ==> %1.2f" % (num2, num2))
+print("四舍五入操作 %s ==> %.2f" % (num2, num2))
