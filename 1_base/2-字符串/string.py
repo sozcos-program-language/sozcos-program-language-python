cfba22541ae9f1b72842d79a570babd35f21f66a
sozcos <sozcos@qq.com>
2024-11-26 21:30:23 +0800

    docs(基础): 字符串的快速格式化

--- 1_base/2-字符串/string.py
+++ 1_base/2-字符串/string.py
@@ -1,5 +1,3 @@
-name = "elsen"
-age = 17
 
 print("我是%s, 今年%s岁" % (name, age))
 
@@ -10,3 +8,7 @@ num2 = 11.345
 print("补位操作 %s ==> %5d" % (num1, num1))
 print("补位四舍五入操作 %s ==> %1.2f" % (num2, num2))
 print("四舍五入操作 %s ==> %.2f" % (num2, num2))
+
+
+# 字符串的快速格式化
+print(f"我的名字是{name}, 今年{age}岁")
