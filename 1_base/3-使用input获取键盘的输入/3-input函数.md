268c4e8f4bb0e86d876920b7c099811bab72f7de
sozcos <sozcos@qq.com>
2024-11-27 17:50:33 +0800

    docs(基础): input函数

---
+++ 1_base/3-使用input获取键盘的输入/3-input函数.md
@@ -0,0 +1,13 @@
+# input函数
+
+使用一个函数接受键盘输入的值
+
+name = input()
+
+```pycon
+print("what is your name?")
+name = input()
+print("Hello, " + name + "!")
+```
+
+
---
+++ 1_base/3-使用input获取键盘的输入/MyInput.py
@@ -0,0 +1,6 @@
+print("what is your name?")
+name = input()
+print("Hello, " + name + "!")
+
+name2= input("Press Enter to exit...")
+print("Goodbye, " + name2 + "!")
