6ed4feb4243bfda75c9b6b5efb67b607d3847a5f
sozcos <sozcos@qq.com>
2024-11-30 10:12:07 +0800

    docs(基础): 文件操作

--- 1_base/8-文件编码/MyFile.py
+++ 1_base/8-文件编码/MyFile.py
@@ -2,9 +2,15 @@
 使用 open 函数操作文件
 """
 
-with open("file.txt", "r", encoding="utf-8") as f:
-     content = f.read()
-     print(content)
+with open("file.txt", "r") as f:
+     read = f.read()
+     print(read)
+
+with open("file.txt", "w", encoding="utf-8") as f:
+     f.write("你好，世界！")
+
+
+
 
 
 
