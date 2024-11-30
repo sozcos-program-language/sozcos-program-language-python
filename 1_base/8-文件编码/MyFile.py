49ddefec59f39f384e159071dbf0f66bfebd834d
sozcos <sozcos@qq.com>
2024-11-30 10:15:32 +0800

    docs(基础): 文件操作练习

--- 1_base/8-文件编码/MyFile.py
+++ 1_base/8-文件编码/MyFile.py
@@ -6,11 +6,16 @@ with open("file.txt", "r") as f:
      read = f.read()
      print(read)
 
-with open("file.txt", "w", encoding="utf-8") as f:
-     f.write("你好，世界！")
+# with open("file.txt", "w", encoding="utf-8") as f:
+#      f.write("你好，世界！")
 
 
-
-
+"""
+统计文件中: itheima 出现的次数
+"""
+with open("file.txt", "r", encoding="utf-8") as f:
+     read = f.read()
+     count = read.count("itheima")
+     print(count)
 
 
