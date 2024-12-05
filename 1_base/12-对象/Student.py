b3fac32fe0c9f36787743ccc6b0f83cf8c7603f0
sozcos <sozcos@qq.com>
2024-12-05 17:25:03 +0800

    docs(基础): 类,对象以及成员方法

---
+++ 1_base/12-对象/Student.py
@@ -0,0 +1,9 @@
+class Student:
+
+    # init 函数就是对象的构造器
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __le__(self, other):
+        return self.age <= other.age
---
+++ 1_base/12-对象/test.py
@@ -0,0 +1,9 @@
+from  Student import Student
+
+
+s1 = Student("hello", 11)
+s2 = Student("hello", 12)
+
+print(s1 < s2)
+
+
